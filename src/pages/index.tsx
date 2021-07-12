b9b95c4149659518f2dce39a5546711ae8d53e01
dev-viniciuss <dev.mvsc@gmail.com>
2021-07-12 15:50:34 -0300

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Dashgo | Home</title>
+      </Head>
+      <h1>Hello Wolrd</h1>
+    </>
+  )
+}
