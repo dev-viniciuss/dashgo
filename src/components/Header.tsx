539660501b185817e8d3b97d8134f3b354996284
dev-viniciuss <dev.mvsc@gmail.com>
2021-07-13 14:00:49 -0300

    feat: criando o Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Flex } from '@chakra-ui/react'
+
+import { Logo } from './Logo'
+
+export function Header() {
+  return(
+    <Flex as="header" w="100%" maxW={1480} h="20" mx="auto" mt="4" px="6" align="center">
+      <Logo fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64" />
+    </Flex>
+  )
+}
