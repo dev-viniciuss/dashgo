fab538ec0173af94df7d062d1c2f6dc67c360f23
dev-viniciuss <dev.mvsc@gmail.com>
2021-07-13 14:00:40 -0300

    feat: criando componente Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,16 @@
+import { Text, TextProps } from '@chakra-ui/react'
+
+interface LogoProps extends TextProps {
+  fontSize: string;
+  fontWeight: string;
+  letterSpacing: string;
+}
+
+export function Logo({ fontSize, fontWeight, letterSpacing, ...rest }: LogoProps) {
+  return(
+    <Text fontSize={fontSize} fontWeight={fontWeight} letterSpacing={letterSpacing} {...rest}>
+      dashgo
+      <Text as="span" ml="1" color="pink.500">.</Text>
+    </Text>
+  )
+}
